3b5fbb0858378e4e49c96426891f6cb724799188
nazyhomayoun <nazyhomayounfar@gmail.com>
2025-12-27 19:58:13 +0330

    chore(db): update DATABASE_URL default to movie_db

--- app/db/database.py
+++ app/db/database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # آدرس دیتابیس را در فایل .env قرار بده
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/dbname")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/movie_db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

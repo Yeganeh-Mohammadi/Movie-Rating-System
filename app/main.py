976acd84722841c87012a5b866c0c72a2d28ca6d
nazyhomayoun <nazyhomayounfar@gmail.com>
2025-12-31 22:15:51 +0330

    Update: main.py to include movie controller router

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from app.controller import movie_controller
+from app.db.database import Base, engine
+from app.models.models import *
 
 app = FastAPI()
 
+Base.metadata.create_all(bind=engine)
+
 app.include_router(movie_controller.router)
+
 
